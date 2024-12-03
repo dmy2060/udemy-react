8d4cf705466b0debaef782ba48effdde66592bd6
dmy2060 <dmy2060@naver.com>
2024-12-03 14:12:36 +0900

    study: [리액트] 교차 state 방지하기

--- 03-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 03-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,19 +6,25 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  /**
+   * app 컴포넌트에서 끌어올려서 사용할 예정
+   * 여기에는 Log 컴포넌트에 필요한 순서 정보가 없으므로
+   * 참고용으로만 두고 재 작성 예정
+   */
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard(prevGameBoard => {
-      const updatedBoard = [
-        ...prevGameBoard.map(innerArray => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard(prevGameBoard => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map(innerArray => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
 
-    onSelectSquare();
-  }
+  //   onSelectSquare();
+  // }
+
   return (
     <ol id='game-board'>
       {gameBoard.map((row, rowIndex) => (
