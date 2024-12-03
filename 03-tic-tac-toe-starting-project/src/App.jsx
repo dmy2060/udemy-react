171080e22d0919fe31d69dcda24b6b23b8a7f48a
eunyoung <eunyoung.park@lemonhc.com>
2024-12-03 16:01:29 +0900

    study: [리액트] state 간소화, 불필요한 state 분별

--- 03-tic-tac-toe-starting-project/src/App.jsx
+++ 03-tic-tac-toe-starting-project/src/App.jsx
@@ -3,18 +3,26 @@ import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
 
+function deriveActionPlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  //const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActionPlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'));
+    //setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'));
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActionPlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
