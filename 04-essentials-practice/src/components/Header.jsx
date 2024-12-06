53ab3a48165116aa3b486d9bab918f128e014bc1
eunyoung <eunyoung.park@lemonhc.com>
2024-12-06 16:34:05 +0900

    study: [리액트] 투자금 계산기 코드 리팩토링

--- 04-essentials-practice/src/components/Header.jsx
+++ 04-essentials-practice/src/components/Header.jsx
@@ -3,7 +3,7 @@ import CalculatorLogo from '../assets/investment-calculator-logo.png';
 export default function Header({ text }) {
   return (
     <header id='header'>
-      <img src={CalculatorLogo} alt='' />
+      <img src={CalculatorLogo} alt='Logo' />
       <h1>{text}</h1>
     </header>
   );
--- 04-essentials-practice/src/components/UserInput.jsx
+++ 04-essentials-practice/src/components/UserInput.jsx
@@ -1,46 +1,63 @@
 export default function UserInput({ number, onChangeNumber }) {
   return (
-    <section id='user-input'>
-      <div className='input-group'>
-        <div>
-          <label htmlFor=''>INITAL INVESTMENT</label>
-          <input
-            type='number'
-            name='INITALINVESTMENT'
-            value={number.INITALINVESTMENT}
-            onChange={onChangeNumber}
-          />
+    <form action='#'>
+      <section id='user-input'>
+        <div className='input-group'>
+          <div>
+            <label>INITAL INVESTMENT</label>
+            <input
+              type='number'
+              name='initialInvestment'
+              value={number.initialInvestment}
+              onChange={onChangeNumber}
+              required
+            />
+          </div>
+          <div>
+            <label>ANINUAL INVESTMENT</label>
+            <input
+              type='number'
+              name='annualInvestment'
+              value={number.annualInvestment}
+              onChange={onChangeNumber}
+              required
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor=''>ANINUAL INVESTMENT</label>
-          <input
-            type='number'
-            name='ANINUALINVESTMENT'
-            value={number.ANINUALINVESTMENT}
-            onChange={onChangeNumber}
-          />
+        <div className='input-group'>
+          <div>
+            <label>EXPECTED RETURN</label>
+            <input
+              type='number'
+              name='expectedReturn'
+              value={number.expectedReturn}
+              onChange={onChangeNumber}
+              required
+            />
+          </div>
+          <div>
+            <label>DURATION</label>
+            <input
+              type='number'
+              name='duration'
+              value={number.duration}
+              onChange={onChangeNumber}
+              required
+            />
+          </div>
         </div>
-      </div>
-      <div className='input-group'>
-        <div>
-          <label htmlFor=''>EXPECTED RETURN</label>
-          <input
-            type='number'
-            name='EXPECTEDRETURN'
-            value={number.EXPECTEDRETURN}
-            onChange={onChangeNumber}
-          />
-        </div>
-        <div>
-          <label htmlFor=''>DURATION</label>
-          <input
-            type='number'
-            name='DURATION'
-            value={number.DURATION}
-            onChange={onChangeNumber}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+    </form>
   );
 }
+
+/**
+ *  다른 방식
+*   name='ANINUALINVESTMENT'
+    value={number.ANINUALINVESTMENT}
+    onChange={onChangeNumber}
+
+    => 이런식으로 name을 선언해서 사용하는 것이 아닌 onChange={(event)=>onChangeNumber('ANINUALINVESTMENT', event.target.value) }
+    이렇게 아예 보낼 때 부터 익명 함수를 통해 이벤트를 전달시켜 받아서 사용해도 됨.
+ *
+ */
