6932ef2f33909d4f374767c84428aad1f5d76a93
dmy2060 <dmy2060@naver.com>
2024-12-06 16:34:05 +0900

    study: [리액트] 투자금 계산기 코드 리팩토링

--- 04-essentials-practice/src/App.jsx
+++ 04-essentials-practice/src/App.jsx
@@ -3,63 +3,35 @@ import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
 import ResultTable from './components/ResultTable.jsx';
 
-import { calculateInvestmentResults } from './util/investment.js';
 const INITNUMBERS = {
-  INITALINVESTMENT: '10000',
-  ANINUALINVESTMENT: '1200',
-  EXPECTEDRETURN: '6',
-  DURATION: '10',
-};
-
-const calculateDataWithInterest = inputData => {
-  const calculateData = calculateInvestmentResults({
-    initialInvestment: Number(inputData.INITALINVESTMENT),
-    annualInvestment: Number(inputData.ANINUALINVESTMENT),
-    expectedReturn: Number(inputData.EXPECTEDRETURN),
-    duration: Number(inputData.DURATION),
-  });
-
-  const updateCalculateData = [...calculateData];
-
-  for (let i = 0; i < updateCalculateData.length; i++) {
-    const data = updateCalculateData[i];
-
-    if (i === 0) {
-      data.investedCapital = data.valueEndOfYear - data.interest;
-    } else {
-      data.investedCapital =
-        updateCalculateData[i - 1].investedCapital + data.annualInvestment;
-    }
-
-    const totalInterestToDate = updateCalculateData
-      .slice(0, i + 1)
-      .reduce((sum, year) => sum + year.interest, 0);
-
-    data.totalInterest = totalInterestToDate;
-  }
-
-  return updateCalculateData;
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
 };
 
 function App() {
   const [number, setNumber] = useState(INITNUMBERS);
-  const [list, setList] = useState(() => calculateDataWithInterest(number));
+  const inputIsValid = number.duration >= 1;
 
   function handleNumberChange(event) {
     const { name, value } = event.target;
-    const updateNum = { ...number, [name]: value };
-
-    setNumber(() => updateNum);
-    setList(calculateDataWithInterest(updateNum));
+    setNumber(number => {
+      return {
+        ...number,
+        [name]: +value,
+      };
+    });
   }
 
   return (
     <>
       <Header text='Investment Calculator' />
-      <form action='#'>
-        <UserInput number={number} onChangeNumber={handleNumberChange} />
-      </form>
-      <ResultTable list={list} />
+      <UserInput number={number} onChangeNumber={handleNumberChange} />
+      {!inputIsValid && (
+        <p className='center'>Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <ResultTable input={number} />}
     </>
   );
 }
--- 04-essentials-practice/src/components/ResultTable.jsx
+++ 04-essentials-practice/src/components/ResultTable.jsx
@@ -1,6 +1,10 @@
-import { formatter } from '../util/investment.js';
-
-export default function ResultTable({ list }) {
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
+export default function ResultTable({ input }) {
+  const resultData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
   return (
     <section>
       <table id='result'>
@@ -14,15 +18,22 @@ export default function ResultTable({ list }) {
           </tr>
         </thead>
         <tbody>
-          {list.map((item, index) => (
-            <tr key={index + 1}>
-              <td>{item.year}</td>
-              <td>{formatter.format(item.valueEndOfYear)}</td>
-              <td>{formatter.format(item.interest)}</td>
-              <td>{formatter.format(item.totalInterest)}</td>
-              <td>{formatter.format(item.investedCapital)}</td>
-            </tr>
-          ))}
+          {resultData.map((item, index) => {
+            const totalInterest =
+              item.valueEndOfYear -
+              item.annualInvestment * item.year -
+              initialInvestment;
+            const totalAmountInvested = item.valueEndOfYear - totalInterest;
+            return (
+              <tr key={index + 1}>
+                <td>{item.year}</td>
+                <td>{formatter.format(item.valueEndOfYear)}</td>
+                <td>{formatter.format(item.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </section>
