6e9cb84ecaadb892bc2156f1e39ea372d3ec2395
dmy2060 <dmy2060@naver.com>
2024-12-10 16:20:09 +0900

    study: [리액트] vanilla css 장.단점 및 사용 방법. 스코프 관련 공부

--- 05-styling/src/components/AuthInputs.jsx
+++ 05-styling/src/components/AuthInputs.jsx
@@ -21,32 +21,34 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id='auth-inputs'>
+      <div className='controls'>
         <p>
           <label>Email</label>
           <input
-            type="email"
+            type='email'
             className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            onChange={event => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
           <label>Password</label>
           <input
-            type="password"
+            type='password'
             className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
+            onChange={event =>
               handleInputChange('password', event.target.value)
             }
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className='actions'>
+        <button type='button' className='text-button'>
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className='button' onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
